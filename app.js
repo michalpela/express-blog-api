107624d629ddf0a46395dbd53f2fe37b22679c20
michalpela <kingmichal26@wp.pl>
2024-12-13 01:36:03 +0100

    added temporary logic in main file

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express');
-const indexRouter = require('./routes/index.js');
 const app = express();
+const indexRouter = require('./routes/index.js');
+const mongoose = require('mongoose');
 
 app.set('views', "views")
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
+
+const Post = require('./models/Post');
+mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
+
+app.post('/admin/createPost', async (req, res) => {
+    try {
+      const {title, content, keyWords} = req.body;
+
+      const newPost = new Post({
+        title,
+        content,
+        keyWords,
+      });
+      await newPost.save();
+
+      res.status(200).json({message:"Post saved successfully"});
+    } catch (error) {
+      res.status(500).json({message:"Error creating Post"});
+    }
+})
+
+
 
 app.use('/', indexRouter)
 
