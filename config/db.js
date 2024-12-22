44c9cc9f075adce9f9d598bc4eacc91069e42539
michalpela <kingmichal26@wp.pl>
2024-12-22 21:24:57 +0100

    create database connection

---
+++ config/db.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config({path:'/config/.env'});
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI, {
+            autoIndex: true,
+        })
+        console.log("Connected to DB");
+    } catch (error) {console.log(error);}
+}
+export default connectToDB;
