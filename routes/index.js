2e257851077a1f8e0b29c52ae5fe0e058abe4229
michalpela <kingmichal26@wp.pl>
2024-12-22 21:28:29 +0100

    change to ES standard

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+import express from 'express';
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express Blog' });
 })
 
-module.exports = router;
+export default router;
