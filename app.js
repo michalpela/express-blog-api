feb31f3126d1197136c66b4eb8667ce26f97160a
michalpela <kingmichal26@wp.pl>
2025-03-17 00:08:40 +0100

    refactor code

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import indexRouter from './routes/index.js';
-import Post from './models/post.js';
+dotenv.config({path: `./config/.env`})
+import index from './routes/index.js';
+import post from './routes/post.js';
 import connectToDB from "./config/db.js";
 
+connectToDB()
 
 const app = express();
-dotenv.config({path: `./config/.env`})
 
 app.set('views', "views")
 app.set('view engine', 'ejs')
@@ -16,26 +17,9 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static("public"))
 
-connectToDB();
 
-app.post('/admin/createPost', async (req, res) => {
-    try {
-      const {title, content, keyWords} = req.body;
-
-      const newPost = new Post({
-        title,
-        content,
-        keyWords,
-      });
-      await newPost.save();
-
-      res.status(200).json({message:"Post saved successfully"});
-    } catch (error) {
-      res.status(500).json({message:"Error creating Post"});
-    }
-})
-
-app.use('/', indexRouter)
+app.use('/', index)
+app.use('/', post)
 
 app.listen(process.env.PORT, () =>{
   console.log(`Server started on port ${process.env.PORT}`);
