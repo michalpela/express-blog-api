fc737acc74c51e9b61bf52b45f9559aa167bc8df
michalpela <kingmichal26@wp.pl>
2025-03-19 22:54:56 +0100

    refactor code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config({path: `./config/.env`})
-import index from './routes/index.js';
+
 import post from './routes/post.js';
 import connectToDB from "./config/db.js";
 
@@ -9,8 +9,6 @@ await connectToDB()
 
 const app = express();
 
-app.set('views', "views")
-app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -18,8 +16,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 
-app.use('/', index)
-app.use('/', post)
+app.use('/api/', post)
 
 app.listen(process.env.PORT, () =>{
   console.log(`Server started on port ${process.env.PORT}`);
